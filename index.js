8253f13f10bcfc28a3754726c9c3d5daf9a729d8
Daniel Succar <danielsuccar@icloud.com>
2019-12-29 20:31:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ function theBeatlesPlay(players, instruments) {
 function johnLennonFacts(facts) {
     var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
     var newFacts = [];
-    var i = 0;
-    while (i < facts.length) {
+    var x = 0;
+    while (x < facts.length) {
         newFacts.push(facts[i] +"!!!");
-        i++;
+        x++;
     }
     return newFacts;
 }
